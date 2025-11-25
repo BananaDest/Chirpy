5c5818842766a96f18b5c3715257749fd5eff81b
Fabian Mena <famega12@gmail.com>
2025-11-24 18:20:24 -0600

    Chirpy

---
+++ README.md
@@ -0,0 +1,3 @@
+ # Chirpy
+
+Backend server created following the guide on boot.dev to learn how to combine Go, backend REST Apis and SQLC tooling.
--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/BananaDest/Chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
-	fileServerHits atomic.Int32
+	fileServerHits  atomic.Int32
+	databaseQueries *database.Queries
+	platform        string
+}
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
+	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	dbQueries := database.New(db)
 	cfg := apiConfig{
-		fileServerHits: atomic.Int32{},
+		fileServerHits:  atomic.Int32{},
+		databaseQueries: dbQueries,
+		platform:        platform,
 	}
 	serveMux := http.NewServeMux()
 	server := &http.Server{Addr: "localhost:8080", Handler: serveMux}
@@ -32,10 +62,69 @@ func main() {
 	serveMux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
 	serveMux.HandleFunc("POST /admin/reset", cfg.resetHandler)
 	serveMux.HandleFunc("POST /api/validate_chirp", cfg.validateChirpHandler)
-	err := server.ListenAndServe()
+	serveMux.HandleFunc("POST /api/users", cfg.createUser)
+	err = server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func (cfg *apiConfig) createUser(res http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	type error struct {
+		Error string `json:"error"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		res.WriteHeader(500)
+		res.Header().Set("Content-Type", "application/json")
+		errorBody := error{
+			Error: fmt.Sprintf("%v", err),
+		}
+		errorData, _ := json.Marshal(errorBody)
+		res.Write(errorData)
+		return
+	}
+	user, err := cfg.databaseQueries.CreateUser(req.Context(), params.Email)
+	if err != nil {
+		log.Printf("Error creating User: %s", err)
+		res.WriteHeader(500)
+		res.Header().Set("Content-Type", "application/json")
+		errorBody := error{
+			Error: fmt.Sprintf("%v", err),
+		}
+		errorData, _ := json.Marshal(errorBody)
+		res.Write(errorData)
+		return
+
+	}
+	userParsed := User{
+		ID:        user.ID,
+		Email:     user.Email,
+		UpdatedAt: user.UpdatedAt,
+		CreatedAt: user.CreatedAt,
+	}
+	dat, err := json.Marshal(userParsed)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		res.WriteHeader(500)
+		res.Header().Set("Content-Type", "application/json")
+		errorBody := error{
+			Error: fmt.Sprintf("%v", err),
+		}
+		errorData, _ := json.Marshal(errorBody)
+		res.Write(errorData)
+		return
+	}
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(201)
+	res.Write(dat)
 }
 
 func (cfg *apiConfig) metricsHandler(res http.ResponseWriter, req *http.Request) {
@@ -55,11 +144,22 @@ func (cfg *apiConfig) metricsHandler(res http.ResponseWriter, req *http.Request)
 }
 
 func (cfg *apiConfig) resetHandler(res http.ResponseWriter, req *http.Request) {
+	if cfg.platform != "dev" {
+		res.WriteHeader(500)
+		return
+	}
+	cfg.fileServerHits.Swap(0)
+	err := cfg.databaseQueries.DeleteUsers(req.Context())
+	if err != nil {
+
+		log.Printf("Error truncating users: %s", err)
+		res.WriteHeader(500)
+		return
+	}
 	res.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	res.WriteHeader(200)
-	cfg.fileServerHits.Swap(0)
 	response := fmt.Sprintf("Hits: %v", cfg.fileServerHits.Load())
-	_, err := res.Write([]byte(response))
+	_, err = res.Write([]byte(response))
 	if err != nil {
 		fmt.Println("error")
 	}
